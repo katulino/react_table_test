258e7133b778586db61f79ec34b047d70a8e8e53
Pavlikova <katpavlik@gmail.com>
2020-07-10 10:22:11 +0200

    Updates

---
+++ precache-manifest.01141660afb6f8041236c11c7128c4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a8af357956e36cef22c5493c34139c",
+    "url": "/react_table_test/index.html"
+  },
+  {
+    "revision": "1a88ef88b9ce01be88c3",
+    "url": "/react_table_test/static/css/main.b5eeedc2.chunk.css"
+  },
+  {
+    "revision": "78f003352c896afedf21",
+    "url": "/react_table_test/static/js/2.d7a702df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_table_test/static/js/2.d7a702df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a88ef88b9ce01be88c3",
+    "url": "/react_table_test/static/js/main.c06f6560.chunk.js"
+  },
+  {
+    "revision": "b9642f41828faa222ce3",
+    "url": "/react_table_test/static/js/runtime-main.dd9a9984.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_table_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
