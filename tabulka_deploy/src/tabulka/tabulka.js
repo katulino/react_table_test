8a767e18a5fb941c0436d185c9a456df1a3bffac
Pavlikova <katpavlik@gmail.com>
2020-07-10 12:36:23 +0200

    novyKod

---
+++ tabulka_deploy/src/tabulka/tabulka.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import HlavickaTabulky from './HlavickaTabulky/hlavickaTabulky';
+import TeloTabulky from './TeloTabulky/teloTabulky'
+import NewUserForm from './PridejUzivatele/Formular_NovyUzivatel'
+
+export default class Tabulka extends Component {
+    render() {
+    		this.formPridat = null
+   // formulář pro přidání uživatele proměnná a funkce
+   		var popisek = "Přidat uživatele"
+         if (this.props.ukazForm ===true) {
+    			{this.formPridat = <NewUserForm pridej={this.props.pridatFunkce}
+                                                        hlavicka = {this.props.hlavicka}/>
+                                                        }
+    			popisek = "Schovat formulář"
+    				}
+    		console.log(`pisuVtabulce ${this.props.telo}`);
+    		console.log(`pisuVtabulce ${this.props.hlavicka}`);
+
+    return (
+    	<div className="tabulka blok">
+    		<h1 id='title'></h1>   		
+    			<table id = 'table'>
+    				<HlavickaTabulky vstup={this.props.hlavicka} />
+    				<TeloTabulky vstup={this.props.telo}
+    				             smazat={this.props.smazat}
+        				     editovat = {this.props.editFunkce}
+        				     />
+    			</table>
+    			<button onClick={() =>this.props.formFunkce()} >{popisek}</button>
+				{this.formPridat}
+    	</div>       
+  		);
+  		}  
+	}
